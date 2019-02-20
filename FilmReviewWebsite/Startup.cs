eb74e112938507d5a3cea418e3ecea425632e03a
BradWartho <p17212092@my365.dmu.ac.uk>
2019-02-20 15:21:03 +0000

    Added MVC for reference

---
+++ FilmReviewWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmReviewWebsite.Startup))]
+namespace FilmReviewWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
