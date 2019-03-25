8ca47c2db55edf8a4685176d2273adffe96a6da8
Christopher Mustapha <P17191500@GWHXX652WIN016W>
2019-03-25 18:54:38 +0000

    Added Actor Class, with all attributes.

---
+++ MovieReviewWebsite/MovieReviewWebsite/Models/clsActors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviewWebsite.Models
+{
+    public class clsActors
+    {
+        public int FilmID { get; set; }
+        public string ActorFirstName { get; set; }
+        public string ActorLastName { get; set; }
+        public Int32 Age { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ActorID { get; set; }
+    }
+}
+        
