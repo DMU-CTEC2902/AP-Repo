188bda8c61a89caaebee095f176af72e3efe44ff
Shepdogg123 <shepdogg123@hotmail.co.uk>
2019-03-26 22:56:26 +0000

    Added a database file Added a Genre class Edited the Home page Edited the Shared layout

---
+++ MovieReviewWebsite/MovieReviewWebsite/Models/clsGenre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviewWebsite.Models
+{
+    public class clsGenre
+    {
+        public int GenreID { get; set; }
+        public string GenreName { get; set; }
+    }
+}
